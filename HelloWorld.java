028fbee29dd05fe4f24e7c980ea2b09c0010dffb
StarkBak <sstechnologyindtec@gmail.com>
2025-02-22 22:01:25 +0430

    new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, java class!");
+        System.out.println("Hello, java class!dfghxdfghdfg");
     }
 }
